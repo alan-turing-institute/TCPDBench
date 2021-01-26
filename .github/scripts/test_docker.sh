dc915783fe77d331c21f5bfd61db76751d6328a2
Gertjan van den Burg <gertjanvandenburg@gmail.com>
2021-01-26 18:58:04 +0000

    test building virtualenvs in docker

--- .github/scripts/test_docker.sh
+++ .github/scripts/test_docker.sh
@@ -12,8 +12,16 @@ echo "Building docker image"
 
 docker build -t alan-turing-institute/tcpdbench .
 
+echo "Creating output directory"
+
 mkdir -p ${GITHUB_WORKSPACE}/analysis/output
+
+echo "Recreating results and checking for differences"
 
 docker run -v ${GITHUB_WORKSPACE}/analysis/output:/TCPDBench/analysis/output \
 	alan-turing-institute/tcpdbench \
 	/bin/bash -c "make clean && make results && git checkout ./analysis/output/rankplots/*.pdf && git diff --exit-code"
+
+echo "Test building the virtual environments"
+
+docker run alan-turing-institute/tcpdbench /bin/bash -c "make venvs"
